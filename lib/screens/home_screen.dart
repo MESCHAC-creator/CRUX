339d3751987d919ae5e606fcf5e756aae4585faf
MESCHAC <kouakouchristevann@gmail.com>
2026-05-09 01:29:31 +0000

    Fix file structure

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,8 +6,8 @@ import '../models/meeting_model.dart';
 import '../services/meeting_service.dart';
 import '../services/auth_service.dart';
 import '../widgets/custom_button.dart';
-import 'meeting_screen.dart';
-import 'login_screen.dart';
+import '../home/meeting_screen.dart';
+import '../home/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   final UserModel user;
