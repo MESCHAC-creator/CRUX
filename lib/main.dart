364cb31c1ffd33fe704117af3cb3431cbdd33427
MESCHAC <kouakouchristevann@gmail.com>
2026-05-09 00:11:48 +0000

    Add firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -20,8 +23,16 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
-          child: Text('Bienvenue sur CRUX !'),
+          child: Text(
+            'CRUX',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
