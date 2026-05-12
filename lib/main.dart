a4e7b9efd898e239a44dcffc779f1fdb11ccba87
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-12 17:29:47 +0000

    Integrate Firebase Auth and create AuthWrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'models/user_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,10 +24,50 @@ class MyApp extends StatelessWidget {
       title: 'CRUX',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xFF2D8CFF)),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const AuthWrapper(),
+    );
+  }
+}
+
+class AuthWrapper extends StatefulWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  State<AuthWrapper> createState() => _AuthWrapperState();
+}
+
+class _AuthWrapperState extends State<AuthWrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            backgroundColor: Color(0xFF1C1C1E),
+            body: Center(
+              child: CircularProgressIndicator(
+                  color: Color(0xFF2D8CFF)),
+            ),
+          );
+        }
+        if (snapshot.hasData && snapshot.data != null) {
+          final firebaseUser = snapshot.data!;
+          final user = UserModel(
+            uid: firebaseUser.uid,
+            name: firebaseUser.displayName ??
+                firebaseUser.email?.split('@')[0] ??
+                'Utilisateur',
+            email: firebaseUser.email ?? '',
+          );
+          return HomeScreen(user: user);
+        }
+        return const SplashScreen();
+      },
     );
   }
 }
