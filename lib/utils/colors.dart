b6577c799f6a5248c16a74fbd6928727afcb41ab
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-14 12:01:55 +0000

    Refactor color constants in AppColors class
    Updated color constants for background and surface shades, and added new mode colors.

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,9 +4,9 @@ class AppColors {
   static const Color primary = Color(0xFF2D8CFF);
   static const Color primaryDark = Color(0xFF1A6FD4);
   static const Color secondary = Color(0xFF00C853);
-  static const Color background = Color(0xFF1C1C1E);
-  static const Color surface = Color(0xFF2C2C2E);
-  static const Color surfaceLight = Color(0xFF3A3A3C);
+  static const Color background = Color(0xFF111318);
+  static const Color surface = Color(0xFF1E2128);
+  static const Color surfaceLight = Color(0xFF2A2D36);
   static const Color error = Color(0xFFFF3B30);
   static const Color white = Color(0xFFFFFFFF);
   static const Color grey = Color(0xFF8E8E93);
@@ -15,4 +15,10 @@ class AppColors {
   static const Color success = Color(0xFF34C759);
   static const Color warning = Color(0xFFFF9500);
   static const Color overlay = Color(0x80000000);
+  
+  // Mode colors
+  static const Color church = Color(0xFF9B59B6);
+  static const Color education = Color(0xFF2ECC71);
+  static const Color enterprise = Color(0xFF3498DB);
+  static const Color coaching = Color(0xFFE67E22);
 }
