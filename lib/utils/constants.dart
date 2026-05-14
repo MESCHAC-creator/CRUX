d85f68e5f19b701f0ff6c5e3068fcc99ea4ecd29
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-14 11:50:00 +0000

    Replace AppColors class with AppConstants class

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,22 +1,13 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primary = Color(0xFF2D8CFF);
-  static const Color primaryDark = Color(0xFF1A6FD4);
-  static const Color secondary = Color(0xFF00C853);
-  static const Color background = Color(0xFF111318);
-  static const Color surface = Color(0xFF1E2128);
-  static const Color surfaceLight = Color(0xFF2A2D36);
-  static const Color error = Color(0xFFFF3B30);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color grey = Color(0xFF8E8E93);
-  static const Color darkGrey = Color(0xFF48484A);
-  static const Color danger = Color(0xFFFF3B30);
-  static const Color success = Color(0xFF34C759);
-  static const Color warning = Color(0xFFFF9500);
-  static const Color overlay = Color(0x80000000);
-  static const Color church = Color(0xFF9B59B6);
-  static const Color education = Color(0xFF2ECC71);
-  static const Color enterprise = Color(0xFF3498DB);
-  static const Color coaching = Color(0xFFE67E22);
+class AppConstants {
+  static const String appName = 'CRUX';
+  static const String tagline = 'Connectez-vous, collaborez, créez';
+  static const String emailHint = 'Email';
+  static const String passwordHint = 'Mot de passe';
+  static const String nameHint = 'Nom complet';
+  static const String loginButton = 'Se connecter';
+  static const String registerButton = 'S\'inscrire';
+  static const String newMeeting = 'Nouvelle réunion';
+  static const String joinMeeting = 'Rejoindre';
+  static const String meetingCode = 'Code ex: ABC123';
+  static const String agoraAppId = '3ed3eb7e29c245df8fcd7eb10a346a3d';
 }
