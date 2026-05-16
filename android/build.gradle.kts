356575157d31e98ffd519370d5939f5c5eb72a04
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-16 11:59:52 +0000

    Refactor build.gradle.kts for project structure

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,85 +1,20 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.example.crux"
-    compileSdk = 36
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    defaultConfig {
-        applicationId = "com.example.crux"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-
-        multiDexEnabled = true
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs["debug"]
-        }
-        debug {
-            isDebuggable = true
-        }
-    }
-
-    packagingOptions {
-        exclude("META-INF/proguard/androidx-*.pro")
-        exclude("META-INF/androidx.*.version")
-    }
-
-    lint {
-        disable.add("MissingDimensionRegistration")
-        disable.add("InvalidPackage")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
-flutter {
-    source = "../.."
+rootProject.buildDir = file("${rootDir}/.gradle")
+
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
 }
 
-dependencies {
-    // AndroidX Core
-    implementation("androidx.core:core:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.multidex:multidex:2.0.1")
+subprojects {
+    project.evaluationDependsOn(':app')
+}
 
-    // Firebase BOM
-    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.firebase:firebase-core")
-
-    // Google Play Services
-    implementation("com.google.android.gms:play-services-basement:18.3.0")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.google.android.gms:play-services-base:18.3.0")
-
-    // WebView & Media
-    implementation("androidx.webkit:webkit:1.7.0")
-    implementation("androidx.exifinterface:exifinterface:1.3.6")
-
-    // Network & HTTP
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-
-    // Testing
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
