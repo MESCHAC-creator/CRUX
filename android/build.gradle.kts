9c162af68231a4c1bf8f691470b1e17e662a6e12
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-16 13:16:13 +0000

    Add Flutter Gradle plugin to build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application") version "8.1.4" apply false
     id("com.android.library") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
 allprojects {
