e6ae97416babbdad6b5088c4ecdba5b7bc3366fb
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-05-16 11:44:36 +0000

    Refactor Gradle Kotlin DSL syntax in build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
-    id "dev.flutter.flutter-gradle-plugin"
-    id "com.google.gms.google-services"
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -26,25 +26,26 @@ android {
         versionName = flutter.versionName
 
         multiDexEnabled = true
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.debug
+            signingConfig = signingConfigs["debug"]
         }
         debug {
-            debuggable = true
+            isDebuggable = true
         }
     }
 
     packagingOptions {
-        exclude 'META-INF/proguard/androidx-*.pro'
+        exclude("META-INF/proguard/androidx-*.pro")
+        exclude("META-INF/androidx.*.version")
     }
 
-    lintOptions {
-        disable 'MissingDimensionRegistration'
+    lint {
+        disable.add("MissingDimensionRegistration")
+        disable.add("InvalidPackage")
     }
 }
 
@@ -53,33 +54,32 @@ flutter {
 }
 
 dependencies {
-    // Core Android
-    implementation "androidx.core:core:1.12.0"
-    implementation "androidx.appcompat:appcompat:1.6.1"
-    implementation "com.google.android.material:material:1.9.0"
+    // AndroidX Core
+    implementation("androidx.core:core:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.multidex:multidex:2.0.1")
 
-    // Firebase
-    implementation platform("com.google.firebase:firebase-bom:32.5.0")
-    implementation "com.google.firebase:firebase-auth"
-    implementation "com.google.firebase:firebase-firestore"
-    implementation "com.google.firebase:firebase-core"
-    implementation "com.google.firebase:firebase-common-ktx"
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-core")
 
     // Google Play Services
-    implementation "com.google.android.gms:play-services-basement:18.3.0"
-    implementation "com.google.android.gms:play-services-auth:20.7.0"
+    implementation("com.google.android.gms:play-services-basement:18.3.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.android.gms:play-services-base:18.3.0")
 
-    // Multidex
-    implementation "androidx.multidex:multidex:2.0.1"
+    // WebView & Media
+    implementation("androidx.webkit:webkit:1.7.0")
+    implementation("androidx.exifinterface:exifinterface:1.3.6")
 
-    // WebView
-    implementation "androidx.webkit:webkit:1.7.0"
+    // Network & HTTP
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
-    // Image handling
-    implementation "androidx.exifinterface:exifinterface:1.3.6"
-
-    // Tests
-    testImplementation "junit:junit:4.13.2"
-    androidTestImplementation "androidx.test:runner:1.5.2"
-    androidTestImplementation "androidx.test.espresso:espresso-core:3.5.1"
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
